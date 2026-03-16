72e944385d428635729a0f2d2e2988566128d69e
jhemmy <jasonhemmy@gmail.com>
2026-03-15 22:00:04 -0500

    add gtag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -50,6 +50,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PHCHFR4Q');` }} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HXW6S1346T" />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-HXW6S1346T');` }} />
         <HeadContent />
       </head>
       <body>
